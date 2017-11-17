33d326f865084d58331bf5dc278d7dacb74c6d9d
Ted Cart <tcart@Teds-MacBook-Pro-2.local>
2017-11-17 13:19:02 -0500

    added functions to js file but did not follow instructions regarding console.log stuff

--- js/main.js
+++ js/main.js
@@ -1,25 +1,42 @@
-console.log("Up and Running!!");
+//console.log("Up and Running!!");
 
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
 
-console.log(cards);
+var checkForMatch = function() {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match!");
+	} else {
+		alert("Sorry, try again.");
+	}
+}
+
+var flipCard = function(cardId) {
+	cardsInPlay.push(cards[cardId]);
+	if (cardsInPlay.length === 2) {
+		checkForMatch();
+		cardsInPlay = []
+	}
+}
+
+flipCard(0);
+flipCard(2);
+
+
+//console.log(cards);
 
 // console.log("User flipped " + cards[0]);
 // console.log("User flipped " + cards[1]);
 
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
 
-console.log("User flipped " + cardsInPlay[0]);
+//console.log("User flipped " + cardsInPlay[0]);
 
-var cardTwo = cards[1];
-cardsInPlay.push(cardTwo);
 
-console.log("User flipped " + cardsInPlay[1]);
 
-if (cardsInPlay[0] === cardsInPlay[1]) {
+//console.log("User flipped " + cardsInPlay[1]);
+
+/*if (cardsInPlay[0] === cardsInPlay[1]) {
 	alert("You found a match!");
 } else {
 	alert("Sorry, try again.");
-}
+}*/
