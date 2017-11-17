af696671be225ba361884bd1a721ad073614d209
Ted Cart <tcart@Teds-MacBook-Pro-2.local>
2017-11-17 15:58:51 -0500

    it kind of works now - click to flip and check for match

--- js/main.js
+++ js/main.js
@@ -2,24 +2,24 @@
 
 var cards = [
 	{
-		rank: "queen";
-		suit: "hearts";
-		cardImage: "images/queen-of-hearts.png";
+		rank: "queen",
+		suit: "hearts",
+		cardImage: "images/queen-of-hearts.png"
 	},
 	{
-		rank: "queen";
-		suit: "diamonds";
-		cardImage: "images/queen-of-diamonds.png";
+		rank: "queen",
+		suit: "diamonds",
+		cardImage: "images/queen-of-diamonds.png"
 	},
 	{
-		rank: "king";
-		suit: "hearts";
-		cardImage: "images/king-of-hearts.png";
+		rank: "king",
+		suit: "hearts",
+		cardImage: "images/king-of-hearts.png"
 	},
 	{
-		rank: "king";
-		suit: "diamonds";
-		cardImage: "images/king-of-diamonds.png";
+		rank: "king",
+		suit: "diamonds",
+		cardImage: "images/king-of-diamonds.png"
 	}
 ];
 var cardsInPlay = [];
@@ -32,7 +32,9 @@ var checkForMatch = function() {
 	}
 }
 
-var flipCard = function(cardId) {
+var flipCard = function() {
+	var cardId = this.getAttribute("data-id");
+	this.setAttribute("src", cards[cardId].cardImage);
 	cardsInPlay.push(cards[cardId].rank);
 	if (cardsInPlay.length === 2) {
 		checkForMatch();
@@ -40,6 +42,17 @@ var flipCard = function(cardId) {
 	}
 }
 
+var createBoard = function(){
+	for (i = 0; i < cards.length; i++) {
+		var cardElement = document.createElement("img");
+		cardElement.setAttribute("src", "images/back.png");
+		cardElement.setAttribute("data-id", i);
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById('game board').appendChild(cardElement);
+	}
+}
+
+createBoard();
 
 //console.log(cards);
 
