f8b658d0247f16a9da1122a05ba4bb8a9156a988
Ted Cart <tcart@Teds-MacBook-Pro-2.local>
2017-11-17 12:50:47 -0500

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,8 +1,25 @@
 console.log("Up and Running!!");
-var cardOne = "king";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "queen";
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+
+console.log(cards);
+
+// console.log("User flipped " + cards[0]);
+// console.log("User flipped " + cards[1]);
+
+var cardOne = cards[0];
+cardsInPlay.push(cardOne);
+
+console.log("User flipped " + cardsInPlay[0]);
+
+var cardTwo = cards[1];
+cardsInPlay.push(cardTwo);
+
+console.log("User flipped " + cardsInPlay[1]);
+
+if (cardsInPlay[0] === cardsInPlay[1]) {
+	alert("You found a match!");
+} else {
+	alert("Sorry, try again.");
+}
