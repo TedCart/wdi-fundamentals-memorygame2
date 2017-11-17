f131e7b39d9527324ac597a0edaadeec30a124c0
Ted Cart <tcart@Teds-MacBook-Pro-2.local>
2017-11-17 13:48:45 -0500

    added object containing card suits rank and file locations

--- js/main.js
+++ js/main.js
@@ -1,6 +1,27 @@
 //console.log("Up and Running!!");
 
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+	{
+		rank: "queen";
+		suit: "hearts";
+		cardImage: "images/queen-of-hearts.png";
+	},
+	{
+		rank: "queen";
+		suit: "diamonds";
+		cardImage: "images/queen-of-diamonds.png";
+	},
+	{
+		rank: "king";
+		suit: "hearts";
+		cardImage: "images/king-of-hearts.png";
+	},
+	{
+		rank: "king";
+		suit: "diamonds";
+		cardImage: "images/king-of-diamonds.png";
+	}
+];
 var cardsInPlay = [];
 
 var checkForMatch = function() {
@@ -12,15 +33,12 @@ var checkForMatch = function() {
 }
 
 var flipCard = function(cardId) {
-	cardsInPlay.push(cards[cardId]);
+	cardsInPlay.push(cards[cardId].rank);
 	if (cardsInPlay.length === 2) {
 		checkForMatch();
 		cardsInPlay = []
 	}
 }
-
-flipCard(0);
-flipCard(2);
 
 
 //console.log(cards);
